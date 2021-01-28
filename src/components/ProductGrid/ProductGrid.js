be04fe189dd40b27dc57d8e1b1c68f1ccbeda02d
Carlos <53526597+carlosgauci@users.noreply.github.com>
2021-01-28 11:04:28 +0100

    combined latest/featured products into 1 component (ProductGrid)

---
+++ src/components/ProductGrid/ProductGrid.js
@@ -0,0 +1,21 @@
+import React from "react"
+import styles from "./ProductGrid.module.scss"
+import ProductCard from "../ProductCard/ProductCard"
+import uuid from "react-uuid"
+
+const ProductGrid = ({ products, title }) => {
+  return (
+    <section className={styles.featured}>
+      <div className={styles.container}>
+        <h3 className={styles.title}>{title}</h3>
+        <section className={styles.grid}>
+          {products.map(product => (
+            <ProductCard product={product} key={uuid()} />
+          ))}
+        </section>
+      </div>
+    </section>
+  )
+}
+
+export default ProductGrid
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,54 @@
 import React from "react"
 
 import Hero from "../components/Hero/Hero"
-import FeaturedProducts from "../components/FeaturedProducts/FeaturedProducts"
-import LatestProducts from "../components/LatestProducts/LatestProducts"
+import ProductGrid from "../components/ProductGrid/ProductGrid"
 
-const IndexPage = () => (
+const IndexPage = ({ data }) => (
   <>
     <Hero />
-    <FeaturedProducts />
-    <LatestProducts />
+    <ProductGrid title={"Featured Products"} products={data.featured.nodes} />
+    <ProductGrid title={"Latest Additions"} products={data.latest.nodes} />
   </>
 )
+
+export const query = graphql`
+  {
+    latest: allContentfulWooferProducts(
+      sort: { fields: createdAt, order: DESC }
+      limit: 4
+    ) {
+      nodes {
+        name
+        category
+        price
+        sku
+        slug
+        image {
+          fluid {
+            ...GatsbyContentfulFluid_withWebp
+          }
+        }
+      }
+    }
+
+    featured: allContentfulWooferProducts(
+      filter: { featured: { eq: true } }
+      limit: 8
+    ) {
+      nodes {
+        name
+        category
+        price
+        sku
+        slug
+        image {
+          fluid {
+            ...GatsbyContentfulFluid_withWebp
+          }
+        }
+      }
+    }
+  }
+`
 
 export default IndexPage
