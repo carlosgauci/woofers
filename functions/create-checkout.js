ba6048cb0248d541d338e7a88d3caa630ce2ede4
Carlos <53526597+carlosgauci@users.noreply.github.com>
2021-01-30 22:58:58 +0100

    added shipping costs

--- functions/create-checkout.js
+++ functions/create-checkout.js
@@ -8,6 +8,7 @@ async function getData(url) {
   return response.json()
 }
 
+// Get the actual items in the cart from the api, to prevent someone messing with prices
 async function getSelectedProducts(items) {
   const inventory = await getData(
     "https://elated-nobel-9eb8d0.netlify.app/api/get-products"
@@ -23,22 +24,33 @@ async function getSelectedProducts(items) {
   return selected
 }
 
+// Create product data for stripe checkout
 const getLineItems = products => {
   return products.map(product => ({
     name: product.name,
     description: product.description,
-    images: [product.image.url],
+    images: product.image && [product.image.url],
     amount: product.price,
     currency: "EUR",
     quantity: product.quantity,
   }))
 }
 
+const addShipping = products => {
+  const newProducts = products
+  newProducts.push({
+    name: "Shipping",
+    price: 950,
+    quantity: 1,
+  })
+  return newProducts
+}
+
 exports.handler = async event => {
   const { items } = JSON.parse(event.body)
   const products = await getSelectedProducts(items)
-  const validatedQuantity = 1
-  const lineItems = getLineItems(products)
+  const productsWithShipping = addShipping(products)
+  const lineItems = getLineItems(productsWithShipping)
 
   console.log(products)
   console.log(lineItems)
@@ -47,7 +59,36 @@ exports.handler = async event => {
     payment_method_types: ["card"],
     billing_address_collection: "auto",
     shipping_address_collection: {
-      allowed_countries: ["US", "CA", "IN"],
+      allowed_countries: [
+        "US",
+        "AT",
+        "BE",
+        "BG",
+        "HR",
+        "CY",
+        "CZ",
+        "DK",
+        "EE",
+        "FI",
+        "FR",
+        "DE",
+        "GR",
+        "HU",
+        "IE",
+        "IT",
+        "LV",
+        "LT",
+        "LU",
+        "MT",
+        "NL",
+        "PL",
+        "PT",
+        "RO",
+        "SK",
+        "SI",
+        "ES",
+        "SE",
+      ],
     },
     success_url: `${process.env.URL}/success`,
     cancel_url: process.env.URL,
