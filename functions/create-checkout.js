b9af48ee39826a890f32741e18d6094609bda2d5
Carlos <53526597+carlosgauci@users.noreply.github.com>
2021-02-01 20:34:29 +0100

    added better shipping cost logic

--- functions/create-checkout.js
+++ functions/create-checkout.js
@@ -43,16 +43,68 @@ const getLineItems = products => {
 // Add shipping cost
 // Shipping starts at 6€ and increases by 2€ for every extra item
 const addShipping = products => {
-  const totalItems = products.reduce(
-    (prev, current) => prev + current.quantity,
-    0
-  )
+  // const totalItems = products.reduce(
+  //   (prev, current) => prev + current.quantity,
+  //   0
+  // )
+
+  // const newProducts = products
+
+  // newProducts.push({
+  //   name: "Shipping",
+  //   price: 400 + 200 * totalItems,
+  //   quantity: 1,
+  // })
+  // return newProducts
+
+  // Configure shipping prices for the first item of each type and additional items of that type (printful's shipping is weired)
+  const tshirtFirst = 400
+  const tshirtAdditional = 150
+  const bagFirst = 400
+  const bagAdditional = 150
+  const laptopFirst = 600
+  const laptopAdditional = 200
+  const mugFirst = 600
+  const mugAdditional = 360
+
+  // Get the quantity of items from each category
+  const tshirtQuantity = products
+    .filter(item => item.category === "T-shirts")
+    .reduce((prev, current) => prev + current.quantity, 0)
+  const bagQuantity = products
+    .filter(item => item.category === "Bags")
+    .reduce((prev, current) => prev + current.quantity, 0)
+  const laptopCoverQuantity = products
+    .filter(item => item.category === "Laptop Covers")
+    .reduce((prev, current) => prev + current.quantity, 0)
+  const mugQuantity = products
+    .filter(item => item.category === "Coffee Mugs")
+    .reduce((prev, current) => prev + current.quantity, 0)
+
+  // Calculate shipping costs for each category and add them up
+  const tshirtCost =
+    tshirtQuantity > 0
+      ? tshirtFirst + (tshirtQuantity - 1) * tshirtAdditional
+      : 0
+
+  const bagCost =
+    bagQuantity > 0 ? bagFirst + (bagQuantity - 1) * bagAdditional : 0
+
+  const laptopCost =
+    laptopCoverQuantity > 0
+      ? laptopFirst + (laptopCoverQuantity - 1) * laptopAdditional
+      : 0
+
+  const mugCost =
+    mugQuantity > 0 ? mugFirst + (mugQuantity - 1) * mugAdditional : 0
+
+  const total = tshirtCost + bagCost + laptopCost + mugCost
 
   const newProducts = products
 
   newProducts.push({
     name: "Shipping",
-    price: 400 + 200 * totalItems,
+    price: total,
     quantity: 1,
   })
   return newProducts
--- src/utils/calculateShipping.js
+++ src/utils/calculateShipping.js
@@ -1,5 +1,46 @@
-// Shipping costs €6 for the first item and +2€ for each extra item
 export default function calculateShipping(cart) {
-  const totalItems = cart.reduce((prev, current) => prev + current.quantity, 0)
-  return 400 + 200 * totalItems
+  // Configure shipping prices for the first item of each type and additional items of that type (printful's shipping is weired)
+  const tshirtFirst = 400
+  const tshirtAdditional = 150
+  const bagFirst = 400
+  const bagAdditional = 150
+  const laptopFirst = 600
+  const laptopAdditional = 200
+  const mugFirst = 600
+  const mugAdditional = 360
+
+  // Get the quantity of items from each category
+  const tshirtQuantity = cart
+    .filter(item => item.category === "T-shirts")
+    .reduce((prev, current) => prev + current.quantity, 0)
+  const bagQuantity = cart
+    .filter(item => item.category === "Bags")
+    .reduce((prev, current) => prev + current.quantity, 0)
+  const laptopCoverQuantity = cart
+    .filter(item => item.category === "Laptop Covers")
+    .reduce((prev, current) => prev + current.quantity, 0)
+  const mugQuantity = cart
+    .filter(item => item.category === "Coffee Mugs")
+    .reduce((prev, current) => prev + current.quantity, 0)
+
+  // Calculate shipping costs for each category and add them up
+  const tshirtCost =
+    tshirtQuantity > 0
+      ? tshirtFirst + (tshirtQuantity - 1) * tshirtAdditional
+      : 0
+
+  const bagCost =
+    bagQuantity > 0 ? bagFirst + (bagQuantity - 1) * bagAdditional : 0
+
+  const laptopCost =
+    laptopCoverQuantity > 0
+      ? laptopFirst + (laptopCoverQuantity - 1) * laptopAdditional
+      : 0
+
+  const mugCost =
+    mugQuantity > 0 ? mugFirst + (mugQuantity - 1) * mugAdditional : 0
+
+  const total = tshirtCost + bagCost + laptopCost + mugCost
+
+  return total
 }
