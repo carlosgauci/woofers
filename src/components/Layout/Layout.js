f633b720033ba34abb23ba155c834d045d35d914
Carlos <53526597+carlosgauci@users.noreply.github.com>
2021-02-04 20:13:40 +0100

    added framer-motion and added page transitions

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,12 +1,33 @@
 import React, { useState } from "react"
 import PropTypes from "prop-types"
+import { motion, AnimatePresence } from "framer-motion"
 
 import Header from "../Header/Header"
 import MobileNav from "../MobileNav/MobileNav"
 import Announcement from "../Announcement/Announcement"
 import Footer from "../Footer/Footer"
 
-const Layout = ({ children }) => {
+const duration = 0.25
+
+const variants = {
+  initial: {
+    opacity: 0,
+  },
+  enter: {
+    opacity: 1,
+    transition: {
+      duration: duration,
+      delay: duration,
+      when: "beforeChildren",
+    },
+  },
+  exit: {
+    opacity: 0,
+    transition: { duration: duration },
+  },
+}
+
+const Layout = ({ children, location }) => {
   const [mobileNav, setMobileNav] = useState(false)
 
   return (
@@ -14,7 +35,17 @@ const Layout = ({ children }) => {
       <Header mobileNav={mobileNav} setMobileNav={setMobileNav} />
       <MobileNav mobileNav={mobileNav} setMobileNav={setMobileNav} />
       <Announcement />
-      <main>{children}</main>
+      <AnimatePresence>
+        <motion.main
+          key={location.pathname}
+          variants={variants}
+          initial="initial"
+          animate="enter"
+          exit="exit"
+        >
+          {children}
+        </motion.main>
+      </AnimatePresence>
       <Footer />
     </>
   )
